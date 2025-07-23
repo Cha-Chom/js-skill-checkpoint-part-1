16261ac61d02b09ef8226907580008da9218779f
Chanisara <chanisara.sra@gmail.com>
2025-07-23 13:42:59 +0700

    feat: q1 - Implement to-do list modifications: add new item, update existing item, remove last item, and display final to-do list.

--- question-1.js
+++ question-1.js
@@ -5,4 +5,19 @@ const myTodo = [
   { id: 3, todo: "Call mom" },
   { id: 4, todo: "Wash dishes" },
 ];
-// เริ่มเขียนโค้ดตรงนี้
+
+//1 เพิ่ม Obj ใหม่
+let id5 = {id: 5, todo: "Walk the dog"};
+myTodo.push(id5);
+
+//2 แก้ไข value ภายใน Obj
+myTodo[3].todo = "Go to the gym";
+
+//3 ลบ Obj สุดท้ายใน array ออก
+myTodo.pop();
+
+//4 นำ myTodo แสดงบนหน้าจอ
+console.log(myTodo);
+
+//5 แสดงข้อมูล Obj สุดท้ายตามฟอร์ม
+console.log(`To-do id: ${myTodo[myTodo.length-1].id}, ${myTodo[myTodo.length-1].todo}`);
